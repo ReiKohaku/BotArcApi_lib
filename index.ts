853e19e62dbc0bfd96c0b7f1c69b0810cde43303
t404owo <tags4owo@gmail.com>
2022-09-02 18:20:09 +0200

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
-export * from "./src/v3"
-export * from "./src/v4"
-export * from "./src/v5"
-export * from "./src/types"
-export * from "./src/util"
+export * from "./src/v3";
+export * from "./src/v4";
+export * from "./src/v5";
+export * from "./src/types";
+export * from "./src/util";
+export * as util from "./src/util";
